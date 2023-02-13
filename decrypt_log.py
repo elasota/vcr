64f26a1f9b82c7d003f06bef13150d5e91cf96aa
elasota <ejlasota@gmail.com>
2023-02-13 09:02:02 -0500

    Faster decode log tool

--- decrypt_log.py
+++ decrypt_log.py
@@ -8,6 +8,10 @@ def parity_bit(n):
         n = n >> 1
     return parity
 
+def parity_2(n):
+    #return parity_bit(n & 0xc3)
+    return (n ^ (n >> 1) ^ (n >> 6) ^ (n >> 7)) & 1
+
 in_file = open(sys.argv[1], "rb")
 in_bytes = in_file.read()
 in_file.close()
@@ -18,7 +22,7 @@ scramble_key = 0xff
 out_bytes = bytearray(num_bytes)
 
 for i in range(0,num_bytes):
-    scramble_key = (scramble_key >> 1) | (parity_bit(scramble_key & 0xc3) << 7)
+    scramble_key = (scramble_key >> 1) | (parity_2(scramble_key) << 7)
     adjust_index = num_bytes - 1 - i
     out_bytes[adjust_index] = in_bytes[adjust_index] ^ scramble_key
 
